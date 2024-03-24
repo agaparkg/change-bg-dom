3fba8d2ec44d7980e51823ab86eccd04fa0c9bcc
Azamat Gaparov <agaparovkg@gmail.com>
2024-03-23 22:11:39 -0700

    Updated readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Change background color DOM
+
+### UI output
+
+![ui](./images/ui.png)
+
+### Click "Change color" button
+
+![change](./images/change.png)
--- script2.js
+++ script2.js
@@ -2,11 +2,4 @@ const btn2 = document.getElementById("btn2");
 
 btn2.addEventListener("click", function () {
   alert("You clicked button #2");
-  //   const deleted = confirm("Really?");
-
-  //   console.log("User wants to delete?", deleted);
-
-  // const age = prompt("Your age?", "");
-
-  // console.log("Age is", age, typeof age);
 });
